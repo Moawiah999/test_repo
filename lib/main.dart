2ec949ebdd5ff050f42c5f19603a5fdf4babcc7c
Moawiah omer  Aldweri <110593822+Moawiah999@users.noreply.github.com>
2023-05-02 18:05:25 +0300

    Modify the main file in the project

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application .
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'This is an application made on the work environment filters',
       home: VideoScreen(),
     ); //
   }
